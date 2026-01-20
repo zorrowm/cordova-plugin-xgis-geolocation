5d8492e53fe4f65cafaa3f852de121a6214796ad
zorrowm <zorrowm@126.com>
2026-01-20 18:12:09 +0800

    修改v0.0.3版本错误

--- www/AdvancedGeolocation.js
+++ www/AdvancedGeolocation.js
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.​
  */
-var exec = require('cordova/exec');
+var exec = cordova.require('cordova/exec');
 
 var AdvancedGeolocation = {
 
